9fa5f09b0b86048a8450f684c3ec82f4b9ee6b96
Leisvan Cordero <rathmA@LIVE.COM>
2018-10-22 18:28:01 -0400

    UIConvert made static

--- UIConvert.cs
+++ UIConvert.cs
@@ -10,14 +10,14 @@ namespace UniversalPlatformTools
     /// <summary>
     /// Convert from base data types to UI data types and vice versa
     /// </summary>
-    public class UIConvert
+    public static class UIConvert
     {
         /// <summary>
         /// Converts the specified boolean value to a <see cref="Visibility"/> value.
         /// </summary>
         /// <param name="value">The valuue to convert from</param>
         /// <returns><see cref="Visibility.Visible"/> if parameter is True, otherwise returns <see cref="Visibility.Collapsed"/></returns>
-        public Visibility ToVisibility(bool value)
+        public static Visibility ToVisibility(bool value)
         {
             return (value) ? Visibility.Visible : Visibility.Collapsed;
         }
@@ -26,7 +26,7 @@ namespace UniversalPlatformTools
         /// </summary>
         /// <param name="visibility">Tge value to convert from</param>
         /// <returns>True if parameter is <see cref="Visibility.Visible"/> otherwise, False.</returns>
-        public bool ToBoolean(Visibility visibility)
+        public static bool ToBoolean(Visibility visibility)
         {
             return (visibility == Visibility.Visible);
         }
